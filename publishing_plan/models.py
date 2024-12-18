79db1e5e64247390fd7791e41c27fc452687914c
woojin-an <dkswoojin@gmail.com>
2024-12-18 17:40:58 +0900

    model 수정

--- publishing_plan/models.py
+++ publishing_plan/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-from books.models import Book
+from contractor.models import Contractor
 
 User = get_user_model()
 
@@ -13,14 +13,19 @@ class PublishingPlan(models.Model):
         ("completed", "Completed"),
     ]
     id: models.BigAutoField = models.BigAutoField(primary_key=True)
-    planning_start_date: models.DateField = models.DateField()
-    planning_end_date: models.DateField = models.DateField()
+    title: models.CharField = models.CharField(max_length=255)  # 기획 제목
+    description: models.TextField = models.TextField(blank=True, null=True, default="")  # 기획 설명
+    start_date: models.DateField = models.DateField()
+    end_date: models.DateField = models.DateField()
     status: models.CharField = models.CharField(
         max_length=20, choices=STATUS_CHOICES, default="not_started"
     )
     user: models.ForeignKey = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True, related_name="publishing_plans"
     )
+    contractors = models.ManyToManyField(  # 저자/번역가
+        Contractor, related_name="publishing_plans", blank=True
+    )
 
     def __str__(self):
-        return f"Publishing Plan for {self.id}"
+        return f"{self.title} - {self.status}"
