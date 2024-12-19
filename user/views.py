45a419ecb5bd0047002fa4efbb344bdf29de6b2e
woojin-an <dkswoojin@gmail.com>
2024-12-20 03:04:53 +0900

    user login 구현

--- user/views.py
+++ user/views.py
@@ -1,55 +1,69 @@
-from django.contrib.auth import authenticate, get_user_model
-from drf_yasg.utils import swagger_auto_schema
-from rest_framework import status
-from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from django.contrib.auth import get_user_model, login
+from django.core import signing
+from django.core.signing import TimestampSigner, SignatureExpired
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, FormView
 
-from .serializers import UserDetailSerializer, UserLoginSerializer, UserSerializer
+from user.forms import SignupForm, LoginForm
+from config.email import send_email
 
 User = get_user_model()
 
 
-class UserSignupView(CreateAPIView):
-    permission_classes = [AllowAny]
-    serializer_class = UserSerializer
+class SignupView(CreateView):
+    form_class = SignupForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('signup_done')
+
+    def form_valid(self, form):
+        user = form.save()
+        # 이메일 발송
+        signer = TimestampSigner()
+        signed_user_email = signer.sign(user.email)
+        signer_dump = signing.dumps(signed_user_email)
+
+        url = f'{self.request.scheme}://{self.request.META["HTTP_HOST"]}/users/verify/?code={signer_dump}'
+
+        subject = '[To Do List App] 이메일 인증을 완료해주세요!'
+        message = f'다음 링크를 클릭해주세요. <a href="{url}"></a>'
+        send_email(subject, message, from_email=None, to_email=user.email)
+
+        return render(
+            self.request,
+            template_name='registration/signup_done.html',
+            context={'user': user, }
+        )
 
 
-class UserLoginView(APIView):
-    permission_classes = [AllowAny]  # 인증 없이 접근 가능
-    serializer_class = UserLoginSerializer
+def verify_email(request):
+    code = request.GET.get('code', '')
 
-    @swagger_auto_schema(
-        request_body=UserLoginSerializer,
-        responses={200: "로그인 성공", 400: "잘못된 요청"},
-    )
-    def post(self, request, *args, **kwargs):
-        # 시리얼라이저로 유저 정보를 보냄
-        serializer = self.serializer_class(data=request.data)
+    signer = TimestampSigner()
+    try:
+        decoded_user_email = signing.loads(code)
+        email = signer.unsign(decoded_user_email, max_age=60 * 30)
+    except (TypeError, SignatureExpired):
+        return render(request, 'registration/verify_failed.html')
 
-        # 시리얼라이저로 유효하지 않다면
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        # 유효하다면 그 검증된 데이터(메일,암호) 가져옴
-        email = serializer.validated_data.get("email")
-        password = serializer.validated_data.get("password")
-
-        # 해당 이메일과 암호로 요청보냄
-        user = authenticate(request, email=email, password=password)
-
-        # 유저가 없다면 HTTP400
-        if user is None:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        # 유저가 있다면 로그인
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    user = get_object_or_404(User, email=email, is_active=False)
+    user.is_active = True
+    user.save()
+    return render(request, 'registration/verify_success.html', {'user': user})
 
 
-class UserDetailView(RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserDetailSerializer
+class LoginView(FormView):
+    form_class = LoginForm
+    template_name = 'registration/login.html'
+    success_url = reverse_lazy('publishing-plan-list')
 
-    def get_object(self):
-        return self.request.user
+    def form_valid(self, form):
+        user = form.get_user()
+        login(self.request, user=user)
+
+        next_page = self.request.GET.get('next')
+        if next_page:
+            return HttpResponseRedirect(next_page)
+
+        return HttpResponseRedirect(self.get_success_url())
