b7f4cd5d13d28a374a267416c0abd8e034b453d0
woojin-an <dkswoojin@gmail.com>
2024-12-19 22:51:09 +0900

    템플릿 추가

--- publishing_plan/urls.py
+++ publishing_plan/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import PublishingPlanViewSet
 
 router = DefaultRouter()
@@ -9,3 +11,6 @@ router.register(r"", PublishingPlanViewSet, basename="publishing-plans")
 urlpatterns = [
     path("", include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
